d260f7dbef9bbf9216d23589f7d5d659230ed1b6
John Romkey <58883+romkey@users.noreply.github.com>
2018-08-19 15:11:11 -0700

    attempt 2 hour and 24 hour charts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @samples = Sample.order(created_at: :desc).limit(250)
+    @samples_2hours = Sample.order(created_at: :desc).where("created_at > ?", Time.now - 2*60*60)
+    @samples_24hours = Sample.order(created_at: :desc).where("created_at > ?", Time.now - 24*60*60)
   end
 end
